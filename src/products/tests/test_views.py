3f23fd124cd18db9805ce13b0ab26d532e63d8fa
BRANYA <tbd2504@gmail.com>
2023-10-17 17:14:58 +0300

    Fix test from ProdctListView

--- src/products/tests/test_views.py
+++ src/products/tests/test_views.py
@@ -1,15 +1,15 @@
 from django.test import TestCase
 from django.urls import reverse
-from django.views.generic import ListView
+from django.views import generic
 
-from ..views import ProductListView
+from .. import views
 
 
 class ProductListViewTest(TestCase):
-    def test_view_inherit_needed_generic_view(self):
-        self.assertTrue(ProductListView, ListView)
+    def test_view_inherit_necessary_generic_view(self):
+        self.assertTrue(issubclass(views.ProductListView, generic.ListView))
 
-    def test_view_use_needed_template(self):
+    def test_view_use_correct_template(self):
         response = self.client.get(reverse('products:list'))
 
         self.assertTemplateUsed(response, 'products/list.html')
