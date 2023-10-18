c4057c5611c8ca6295837dea817db7ff14088805
BRANYA <tbd2504@gmail.com>
2023-10-18 14:36:51 +0300

    Refactor products tests

--- src/products/tests/test_model.py
+++ src/products/tests/test_model.py
@@ -13,25 +13,29 @@ def create_test_product() -> Product:
     return Product.objects.create(name='name', slug='slug', price=9999)
 
 
+def get_field(model, field_name: str):
+    return model._meta.get_field(field_name)
+
+
 class ProductTest(TestCase):
     def test_product_inherit_needed_model_mixin(self):
         self.assertTrue(issubclass(Product, NameModelMixin))
         self.assertTrue(issubclass(Product, DateModelMixin))
 
     def test_price_field(self):
-        field: models.DecimalField = Product._meta.get_field('price')
+        field: models.DecimalField = get_field(Product, 'price')
 
         self.assertIsInstance(field, models.DecimalField)
 
     def test_specifications_field(self):
-        field: RichTextField = Product._meta.get_field('specifications')
+        field: RichTextField = get_field(Product, 'specifications')
 
         self.assertIsInstance(field, RichTextField)
         self.assertTrue(field.null)
         self.assertTrue(field.blank)
 
     def test_description_field(self):
-        field: RichTextField = Product._meta.get_field('description')
+        field: RichTextField = get_field(Product, 'description')
 
         self.assertIsInstance(field, RichTextField)
         self.assertTrue(field.null)
