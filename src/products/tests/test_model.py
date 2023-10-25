8721e2e65b1ad97490e480e0e5658a98dd703ffa
BRANYA <tbd2504@gmail.com>
2023-10-25 20:23:12 +0300

    Change TestCase -> ModelTest

--- src/products/tests/test_model.py
+++ src/products/tests/test_model.py
@@ -1,9 +1,9 @@
 from base.models import DateModelMixin, NameModelMixin
+from base.unit_tests.model_test import ModelTest
 
 from ckeditor.fields import RichTextField
 
 from django.db import models
-from django.test import TestCase
 from django.urls import reverse
 
 from ..models import Product
@@ -14,36 +14,32 @@ def create_test_product(name='name', slug='slug', price=9999) -> Product:
     return Product.objects.create(name=name, slug=slug, price=price)
 
 
-def get_field(model, field_name: str):
-    return model._meta.get_field(field_name)
-
-
-class ProductTest(TestCase):
+class ProductTest(ModelTest):
     def test_product_inherit_needed_model_mixin(self):
         self.assertTrue(issubclass(Product, NameModelMixin))
         self.assertTrue(issubclass(Product, DateModelMixin))
 
     def test_price_field(self):
-        field: models.DecimalField = get_field(Product, 'price')
+        field: models.DecimalField = self.get_field(Product, 'price')
 
         self.assertIsInstance(field, models.DecimalField)
 
     def test_specifications_field(self):
-        field: RichTextField = get_field(Product, 'specifications')
+        field: RichTextField = self.get_field(Product, 'specifications')
 
         self.assertIsInstance(field, RichTextField)
         self.assertTrue(field.null)
         self.assertTrue(field.blank)
 
     def test_description_field(self):
-        field: RichTextField = get_field(Product, 'description')
+        field: RichTextField = self.get_field(Product, 'description')
 
         self.assertIsInstance(field, RichTextField)
         self.assertTrue(field.null)
         self.assertTrue(field.blank)
 
     def test_image_field(self):
-        field: models.ImageField = get_field(Product, 'image')
+        field: models.ImageField = self.get_field(Product, 'image')
 
         self.assertIsInstance(field, models.ImageField)
         self.assertTrue(field.null)
