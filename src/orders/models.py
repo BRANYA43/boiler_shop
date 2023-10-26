371bb67f68500138c5ef7ffdd0ac4155cad6ed7f
BRANYA <tbd2504@gmail.com>
2023-10-26 14:49:50 +0300

    Add Customer Order OrderProduct models

--- src/orders/models.py
+++ src/orders/models.py
@@ -1,6 +1,12 @@
+from uuid import uuid4
+
+from base.models import DateModelMixin
+
 from django.db import models
 
 from orders.validators import validate_phone
+
+from products.models import Product
 
 
 class PhoneField(models.CharField):
@@ -8,3 +14,22 @@ class PhoneField(models.CharField):
 
     def __init__(self, *args, max_length=20, **kwargs):
         super().__init__(*args, max_length=max_length, **kwargs)
+
+
+class Customer(DateModelMixin):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = PhoneField()
+
+
+class Order(DateModelMixin):
+    uuid = models.UUIDField(default=uuid4)
+    customer = models.ForeignKey(to=Customer, on_delete=models.PROTECT, related_name='orders')
+
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(to=Order, on_delete=models.CASCADE, related_name='products')
+    product = models.ForeignKey(to=Product, on_delete=models.SET_NULL, related_name='order_products', null=True)
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    quantity = models.PositiveIntegerField()
---
+++ src/orders/tests/test_models.py
@@ -0,0 +1,97 @@
+import uuid
+
+from base.models import DateModelMixin
+from base.unit_tests.model_test import ModelTest
+
+from django.db import models
+
+from products.models import Product
+
+from ..models import Customer, Order, OrderProduct, PhoneField
+
+
+def create_test_customer(first_name='First name', last_name='Last name', phone='38(050) 555 55-55') -> Customer:
+    return Customer.objects.create(first_name=first_name, last_name=last_name, phone=phone)
+
+
+def create_test_order(customer: Customer) -> Order:
+    return Order.objects.create(customer=customer)
+
+
+def create_test_order_product(order: Order, product: Product, name=None, price=None, quantity=1) -> OrderProduct:
+    if name:
+        product.name = name
+        product.save()
+    if price:
+        product.price = price
+        product.save()
+
+    name = product.name
+    price = product.price
+    return OrderProduct.objects.create(order=order, product=product, name=name, price=price, quantity=quantity)
+
+
+class OrderModelTest(ModelTest):
+    def test_model_inherit_necessary_mixin(self):
+        self.assertTrue(issubclass(Order, DateModelMixin))
+
+    def test_model_has_uuid_field(self):
+        field: models.UUIDField = self.get_field(Order, 'uuid')
+
+        self.assertIsInstance(field, models.UUIDField),
+        self.assertIs(field.default, uuid.uuid4)
+
+    def test_model_has_customer_field(self):
+        field: models.ForeignKey = self.get_field(Order, 'customer')
+
+        self.assertIsInstance(field, models.ForeignKey)
+
+
+class OrderProductModelTest(ModelTest):
+    def test_model_has_order_field(self):
+        field: models.ForeignKey = self.get_field(OrderProduct, 'order')
+
+        self.assertIsInstance(field, models.ForeignKey)
+
+    def test_model_has_product_field(self):
+        field: models.ForeignKey = self.get_field(OrderProduct, 'product')
+
+        self.assertIsInstance(field, models.ForeignKey)
+
+    def test_model_has_name_field(self):
+        field: models.CharField = self.get_field(OrderProduct, 'name')
+
+        self.assertIsInstance(field, models.CharField)
+        self.assertEqual(field.max_length, 50)
+
+    def test_model_has_price_field(self):
+        field: models.DecimalField = self.get_field(OrderProduct, 'price')
+
+        self.assertIsInstance(field, models.DecimalField)
+
+    def test_model_has_quantity_field(self):
+        field: models.PositiveIntegerField = self.get_field(OrderProduct, 'quantity')
+
+        self.assertIsInstance(field, models.PositiveIntegerField)
+
+
+class CustomerModelTest(ModelTest):
+    def test_model_inherit_necessary_mixin(self):
+        self.assertTrue(issubclass(Customer, DateModelMixin))
+
+    def test_model_has_first_name_field(self):
+        field: models.CharField = self.get_field(Customer, 'first_name')
+
+        self.assertIsInstance(field, models.CharField)
+        self.assertEqual(field.max_length, 50)
+
+    def test_model_has_last_name_field(self):
+        field: models.CharField = self.get_field(Customer, 'last_name')
+
+        self.assertIsInstance(field, models.CharField)
+        self.assertEqual(field.max_length, 50)
+
+    def test_model_has_phone_field(self):
+        field: PhoneField = self.get_field(Customer, 'phone')
+
+        self.assertIsInstance(field, PhoneField)
