8a2be0a2fd10d8cf5d10572e2869d861c7bd65bc
BRANYA <tbd2504@gmail.com>
2023-10-26 19:07:37 +0300

    Add CustomerForm

---
+++ src/orders/forms.py
@@ -0,0 +1,24 @@
+import re
+
+from django import forms
+
+from .models import Customer
+
+
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+        exclude = ('created', 'updated')
+
+    def clean_phone(self):
+        data = self.cleaned_data['phone']
+        data = ''.join(re.findall(r'\d+', data))
+        if data.startswith('38'):
+            return f'38({data[2:5]}) {data[5:8]} {data[8:10]}-{data[10:12]}'
+        return f'38({data[:3]}) {data[3:6]} {data[6:8]}-{data[8:10]}'
+
+    def save(self, commit=True):
+        if Customer.objects.filter(phone=self.cleaned_data['phone']):
+            return Customer.objects.get(phone=self.cleaned_data['phone'])
+        return super().save(commit=commit)
---
+++ src/orders/tests/test_forms.py
@@ -0,0 +1,44 @@
+from django.test import TestCase
+
+from .test_models import create_test_customer
+from ..forms import CustomerForm
+from ..models import Customer
+
+
+class CustomerFormTest(TestCase):
+    def setUp(self) -> None:
+        self.data = {'first_name': 'First name', 'last_name': 'Last name', 'phone': '0505555555'}
+
+    def test_form_changes_phone_for_correct_format_by_pattern(self):
+        form = CustomerForm(data={'phone': '0505555555'})
+        form.is_valid()
+        self.assertEqual(form.cleaned_data['phone'], '38(050) 555 55-55')
+
+    def test_form_save_returns_new_customer(self):
+        form = CustomerForm(data=self.data)
+        form.is_valid()
+
+        self.assertEqual(0, Customer.objects.count())
+
+        form.save()
+
+        self.assertEqual(1, Customer.objects.count())
+
+    def test_form_save_doesnt_create_new_customer_if_customer_with_such_phone_is_existing(self):
+        create_test_customer()
+        form = CustomerForm(data=self.data)
+        form.is_valid()
+
+        self.assertEqual(1, Customer.objects.count())
+
+        form.save()
+
+        self.assertEqual(1, Customer.objects.count())
+
+    def test_form_save_returns_existing_customer_if_customer_with_such_phone_is_existing(self):
+        existing_customer = create_test_customer()
+        form = CustomerForm(data=self.data)
+        form.is_valid()
+        form_customer = form.save()
+
+        self.assertEqual(form_customer.pk, existing_customer.pk)
