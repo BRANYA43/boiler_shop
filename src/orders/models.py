86fb710b6e81366fe7050414d27e5a67dc50155a
BRANYA <tbd2504@gmail.com>
2023-10-26 14:26:50 +0300

    Add PhoneField

--- src/orders/models.py
+++ src/orders/models.py
@@ -1,1 +1,10 @@
-# Create your models here.
+from django.db import models
+
+from orders.validators import validate_phone
+
+
+class PhoneField(models.CharField):
+    default_validators = [validate_phone]
+
+    def __init__(self, *args, max_length=20, **kwargs):
+        super().__init__(*args, max_length=max_length, **kwargs)
