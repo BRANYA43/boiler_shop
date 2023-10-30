f56bb229cc6fc70dfd5c9ac6965fbea046a5e774
BRANYA <tbd2504@gmail.com>
2023-10-30 13:35:53 +0200

    Add user can order product form product detail

--- src/base/functional_tests/test_orders_and_cart.py
+++ src/base/functional_tests/test_orders_and_cart.py
@@ -52,3 +52,45 @@ class OrdersAndCartTest(FunctionalTest):
 
         # Site show message about success making order
         self.wait_for_check_current_url(reverse('orders:success_making_order'))
+
+    def test_user_can_order_a_product_form_product_detail(self):
+        product = create_test_product()
+
+        # User go to detail of chosen product
+        self.browser.get(self.live_server_url + product.get_absolute_url())
+        self.wait_for_check_current_url(product.get_absolute_url())
+
+        # User click on Buy
+        self.browser.find_element(By.NAME, 'buy').click()
+        # - Button change name to Added to cart
+        self.wait_for(lambda: self.browser.find_element(By.NAME, 'added_to_cart'))
+
+        # User go to cart
+        self.browser.find_element(By.NAME, 'cart').click()
+        self.wait_for_check_current_url(reverse('carts:cart'))
+
+        # User check what his chosen product is here
+        self.assertIn(product.name, self.browser.find_element(By.TAG_NAME, 'h3').text)
+
+        # User click on make order and go to make order page
+        self.browser.find_element(By.NAME, 'make_order').click()
+        self.wait_for_check_current_url(reverse('orders:make_order'))
+
+        # User fill customer form
+        first_name = self.browser.find_element(By.NAME, 'first_name')
+        first_name.send_keys('First name')
+        first_name.send_keys(Keys.ENTER)
+
+        last_name = self.browser.find_element(By.NAME, 'last_name')
+        last_name.send_keys('Last name')
+        last_name.send_keys(Keys.ENTER)
+
+        phone = self.browser.find_element(By.NAME, 'phone')
+        phone.send_keys('+380505555555')
+        phone.send_keys(Keys.ENTER)
+
+        # User click on finish order
+        self.browser.find_element(By.NAME, 'finish_order').click()
+
+        # Site show message about success making order
+        self.wait_for_check_current_url(reverse('orders:success_making_order'))
