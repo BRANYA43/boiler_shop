7dffea1ff95d19b3e75541384d1ab683c73c462e
BRANYA <tbd2504@gmail.com>
2023-10-30 15:35:29 +0200

    Refactor navbar FT to add go_to_page_by_navbar

--- src/base/functional_tests/test_navbar.py
+++ src/base/functional_tests/test_navbar.py
@@ -2,30 +2,20 @@ from base.functional_tests.functional_test import FunctionalTest
 
 from django.urls import reverse
 
-from selenium.webdriver.common.by import By
-
 
 class NavbarTest(FunctionalTest):
     def test_navbar_item_of_home_redirects_to_home_page(self):
         self.browser.get(self.live_server_url)
-        self.browser.find_element(By.NAME, 'home').click()
-
-        self.wait_for_check_current_url(reverse('home'))
+        self.go_to_page_by_navbar('home', reverse('home'))
 
     def test_navbar_brand_redirects_to_home_page(self):
         self.browser.get(self.live_server_url)
-        self.browser.find_element(By.NAME, 'brand').click()
-
-        self.wait_for_check_current_url(reverse('home'))
+        self.go_to_page_by_navbar('brand', reverse('home'))
 
     def test_navbar_item_of_product_list_redirects_to_product_list_page(self):
         self.browser.get(self.live_server_url)
-        self.browser.find_element(By.NAME, 'product_list').click()
-
-        self.wait_for_check_current_url(reverse('products:list'))
+        self.go_to_page_by_navbar('product_list', reverse('products:list'))
 
     def test_navbar_item_of_cart_redirects_to_cart_page(self):
         self.browser.get(self.live_server_url)
-        self.browser.find_element(By.NAME, 'cart').click()
-
-        self.wait_for_check_current_url(reverse('carts:cart'))
+        self.go_to_page_by_navbar('cart', reverse('carts:cart'))
